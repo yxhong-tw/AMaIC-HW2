570b3d99d358aad2387633e8b5391e0be0981932
yxhong-tw <yxhong.tw@gmail.com>
2024-03-24 22:14:10 +0800

    Finish the question 3-a

---
+++ q3_a.py
@@ -0,0 +1,40 @@
+import numpy
+import scipy
+
+import matplotlib.pyplot as plt
+
+
+def get_lagrange_polynomial_func(x: numpy.ndarray, y: numpy.ndarray):
+    return scipy.interpolate.lagrange(x, y)
+
+
+def print_lagrange_polynomial_figure(x: numpy.ndarray, y: numpy.ndarray,
+                                     x_new: numpy.ndarray,
+                                     y_new: numpy.ndarray):
+    figure = plt.figure(figsize=(10, 8))
+
+    plt.plot(x, y, label="Original")
+    plt.plot(x_new, y_new, label="New")
+
+    plt.title("Lagrange Polynomial")
+    plt.grid()
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.legend(loc="best")
+    plt.show()
+
+
+def main():
+    x = numpy.array([0, 1.2, 2.5, 3.2, 4])
+    y = numpy.array([5, 4.1, 3.8, 4.2, 6])
+
+    lp_func = get_lagrange_polynomial_func(x, y)
+
+    x_new = numpy.array([item for item in numpy.arange(0, 4, 0.2)])
+    y_new = lp_func(x_new)
+
+    print_lagrange_polynomial_figure(x, y, x_new, y_new)
+
+
+if __name__ == "__main__":
+    main()
