ff7e3223ef0b1adbbf3fff0522e3bb9f2269ab3a
yxhong-tw <yxhong.tw@gmail.com>
2024-03-24 22:29:02 +0800

    Update the unproperly named variable

--- q3_b.py
+++ q3_b.py
@@ -12,10 +12,10 @@ def main():
     x = numpy.array([0, 1.2, 2.5, 3.2, 4])
     y = numpy.array([5, 4.1, 3.8, 4.2, 6])
 
-    lp_func = get_cubic_spline_func(x=x, y=y)
+    cs_func = get_cubic_spline_func(x=x, y=y)
 
     x_new = numpy.array([item for item in numpy.arange(0, 4, 0.2)])
-    y_new = lp_func(x_new)
+    y_new = cs_func(x_new)
 
     print_figure(x=x, y=y, x_new=x_new, y_new=y_new, figure_name="Cubic Spline")
 
