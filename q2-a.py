187d340a7f66c2672a2750a6e2f2f1cae55ca23f
yxhong-tw <yxhong.tw@gmail.com>
2024-03-24 20:55:24 +0800

    Finish the question 2-a

---
+++ q2-a.py
@@ -0,0 +1,22 @@
+import numpy
+
+
+def calculate_inverse_matrix(mat: numpy.ndarray) -> numpy.ndarray:
+    return numpy.linalg.inv(mat)
+
+
+def calculate_x_matrix(A_mat: numpy.ndarray, b_mat: numpy.ndarray) -> numpy.ndarray:
+    A_inv_mat = calculate_inverse_matrix(A_mat)
+    return numpy.dot(A_inv_mat, b_mat)
+
+
+def main():
+    A_mat = numpy.array([[2, -3, 1], [3, 2, -5], [1, 4, -1]])
+    b_mat = numpy.array([[1], [-1], [2]])
+
+    x_mat = calculate_x_matrix(A_mat, b_mat)
+    print(f"x matrix: {x_mat}")
+
+
+if __name__ == "__main__":
+    main()
