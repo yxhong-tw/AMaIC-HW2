0b2e985636a8372a094b909944fda6131ae9adf6
yxhong-tw <yxhong.tw@gmail.com>
2024-03-24 22:22:55 +0800

    Finish the question 3-b

---
+++ q3_b.py
@@ -0,0 +1,24 @@
+import numpy
+import scipy
+
+from q3_a import print_figure
+
+
+def get_cubic_spline_func(x: numpy.ndarray, y: numpy.ndarray):
+    return scipy.interpolate.CubicSpline(x, y)
+
+
+def main():
+    x = numpy.array([0, 1.2, 2.5, 3.2, 4])
+    y = numpy.array([5, 4.1, 3.8, 4.2, 6])
+
+    lp_func = get_cubic_spline_func(x, y)
+
+    x_new = numpy.array([item for item in numpy.arange(0, 4, 0.2)])
+    y_new = lp_func(x_new)
+
+    print_figure(x, y, x_new, y_new, "Cubic Spline")
+
+
+if __name__ == "__main__":
+    main()
